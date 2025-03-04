e7c1789d8345eb6c465d2ab002cf8c96c94f2e8a
spdskatr <25078279+spdskatr@users.noreply.github.com>
2025-03-04 11:39:44 +0000

    Feat: initial commit

---
+++ Fp/Lean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Fp.Lean` library.
+-- Import modules here that should be built as part of the library.
+import Fp.Lean.Basic
---
+++ Main.lean
@@ -0,0 +1,7 @@
+import Fp.Lean
+
+#eval (2 + 3)
+#check Int
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
