f7f1b04341cb521b150f1081f65b8ce6d0f21f1a
spdskatr <25078279+spdskatr@users.noreply.github.com>
2025-04-23 15:16:02 +0100

    Also prove addition theorems manually

--- Fp/Addition.lean
+++ Fp/Addition.lean
@@ -57,18 +57,34 @@ def e_add (a b : EFixedPoint w e) : EFixedPoint (w+1) e :=
 def add (he : 0 < e) (a b : PackedFloat e s) : PackedFloat e s :=
   round _ _ (e_add (a.toEFixed he) (b.toEFixed he))
 
-theorem FixedPoint_add_comm (a b : FixedPoint 16 8)
+theorem FixedPoint_add_comm' (a b : FixedPoint 16 8)
   : (f_add a b).equal (f_add b a) := by
   simp [f_add]
   bv_decide +acNf
 
-theorem EFixedPoint_add_comm (a b : EFixedPoint 16 8)
+theorem EFixedPoint_add_comm' (a b : EFixedPoint 16 8)
   : (e_add a b).equal_or_nan (e_add b a) := by
   simp [e_add, f_add]
   bv_decide +acNf
 
-theorem PackedFloat_add_comm (a b : PackedFloat 5 2)
+theorem PackedFloat_add_comm' (a b : PackedFloat 5 2)
   : (add (by omega) a b) = (add (by omega) b a) := by
   apply PackedFloat.inj
   simp [add, e_add, f_add, round, PackedFloat.toEFixed, -BitVec.shiftLeft_eq', -BitVec.ushiftRight_eq']
   bv_check "Addition.lean-PackedFloat_add_comm-75-2.lrat"
+
+theorem FixedPoint_add_comm (a b : FixedPoint 34 16)
+  : (f_add a b) = (f_add b a) := by
+  apply FixedPoint.inj
+  simp [f_add]
+  bv_decide +acNf
+
+theorem EFixedPoint_add_comm (a b : EFixedPoint 34 16)
+  : (e_add a b) = (e_add b a) := by
+  simp [e_add]
+  cases ha2 : a.state <;> cases hb2 : b.state <;>
+    simp <;> simp_all only [eq_comm, FixedPoint_add_comm]
+
+theorem PackedFloat_add_comm (a b : PackedFloat 5 2)
+  : (add (by omega) a b) = (add (by omega) b a) := by
+  simp [add, EFixedPoint_add_comm]
