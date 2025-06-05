a76d20daec935894cd8109e881d7b2a3d4c189c4
spdskatr <25078279+spdskatr@users.noreply.github.com>
2025-06-05 03:00:54 +0100

    Add new hardest theorem

--- Fp/Theorems/ExtraTheorems.lean
+++ Fp/Theorems/ExtraTheorems.lean
@@ -254,3 +254,16 @@ theorem remainder_doubling_homogeneous (a b : PackedFloat 5 2)
   : doubleRNE (remainder a b) = remainder (doubleRNE a) (doubleRNE b) := by
   bv_float_normalize
   bv_decide
+
+/--
+Remainder is degree 1 homogeneous with respect to multiplication, when said
+multiplication is exact.
+-/
+theorem remainder_doubling_homogeneous (a b c : PackedFloat 5 2)
+  (ha : isExactFloat 5 2 (e_mul a.toEFixed c.toEFixed) ∧ ¬a.isInfinite)
+  (hb : isExactFloat 5 2 (e_mul b.toEFixed c.toEFixed) ∧ ¬b.isInfinite)
+  (hc : isExactFloat 5 2 (e_mul (remainder a b).toEFixed c.toEFixed)
+    ∧ ¬c.isZero ∧ ¬c.isInfinite)
+  : mul (remainder a b) c .RNE = remainder (mul a c .RNE) (mul b c .RNE) := by
+  bv_float_normalize
+  bv_decide (timeout := 300)
